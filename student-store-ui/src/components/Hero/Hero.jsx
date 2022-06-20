8ec3281288c1e50c1df3fc178772b5776c4749b1
Rodrigo Teran <rodrigo.teranhernandez@gmail.com>
2022-06-20 13:29:56 -0700

    feat(Hero): add Hero.jsx milestones

--- student-store-ui/src/components/Hero/Hero.jsx
+++ student-store-ui/src/components/Hero/Hero.jsx
@@ -1,5 +1,10 @@
 export default function Hero() {
     return (
-        <div></div>
+        <div className="hero">
+            <p className="intro">
+                Welcome!
+            </p>
+            <img src="/student_store_icon.18e5d61a.svg" className="hero-img" alt="Hero Img" />
+        </div>
     )
 }
