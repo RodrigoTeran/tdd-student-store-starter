d4b5f26d73984a956bec6f5a04fa61e6d2501b88
Rodrigo Teran <rodrigo.teranhernandez@gmail.com>
2022-06-20 13:21:42 -0700

    feat(NavBar): add NavBar milestones

---
+++ student-store-ui/src/components/Logo/Logo.jsx
@@ -0,0 +1,12 @@
+import {} from "react";
+import { Link } from "react-router-dom";
+
+export default function Logo(){
+    return (
+        <div className="logo">
+            <Link to="/">
+                <img src="./codepath.f1b3e41a.svg" alt="CodePath Logo" />
+            </Link>
+        </div>
+    )
+}
